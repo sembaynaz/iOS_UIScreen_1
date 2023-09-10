938f6691b0aab2510d1ba6ff5f17df64685cc9c9
Naz <nazerke.sembay11@mail.ru>
2023-09-10 19:34:09 +0600

    fixed

--- UIScreens/TabBarController.swift
+++ UIScreens/TabBarController.swift
@@ -33,6 +33,7 @@ class TabBarController: UITabBarController {
         )
         ]
     }
+    
     private func generateVC(viewController: UIViewController, image: UIImage?) -> UIViewController {
         viewController.tabBarItem.image = image
         return viewController
@@ -43,8 +44,18 @@ class TabBarController: UITabBarController {
         tabBar.alpha = 1
         
         let lineView = UIView()
-        lineView.backgroundColor = UIColor(red: 185/255, green: 193/255, blue: 217/255, alpha: 0.5)
-        lineView.frame = CGRect(x: 0, y: 0, width: tabBar.frame.size.width, height: 1)
+        lineView.backgroundColor = UIColor(
+            red: 185/255,
+            green: 193/255,
+            blue: 217/255,
+            alpha: 0.5
+        )
+        lineView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: tabBar.frame.size.width,
+            height: 1
+        )
         
         tabBar.addSubview(lineView)
     }
